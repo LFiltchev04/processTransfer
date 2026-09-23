654d48bd50ebe187f26d82b6bd95121e95ef5dc4
lachezar <llf230@aubg.edu>
2026-09-23 00:07:21 +0000

    nah, it aint done, going to bed, also not sure about the len, the for loop might have to be dropped in dataWrite

--- src/transferPushSvc.cpp
+++ src/transferPushSvc.cpp
@@ -311,6 +311,12 @@ int http2PushService::dataWrite(nghttp2_session *session, nghttp2_frame *frame, 
 
             if(wrtCtxRef != nullptr) {
 
+                io_uring_sqe* sqeWriteFrame = io_uring_get_sqe(&ring);
+                sqeWriteFrame->flags = IOSQE_IO_LINK;
+                sqeWriteFrame->user_data = reinterpret_cast<uint64_t>(wrtCtxRef);
+                io_uring_prep_write(sqeWriteFrame, ctx->outgoingFd, framehd, sizeof(framehd), 0);
+
+
                 for(int x = length; x >= SIXTYFOUR_KB; x -= SIXTYFOUR_KB){
                     wrtCtxRef->openFd = open(dentry->d_name, O_RDONLY);
             
@@ -319,11 +325,11 @@ int http2PushService::dataWrite(nghttp2_session *session, nghttp2_frame *frame, 
                     io_uring_sqe* sqePipeRead = io_uring_get_sqe(&ring);
                     sqePipeRead->flags = IOSQE_IO_LINK;
                     sqePipeRead->user_data = reinterpret_cast<uint64_t>(wrtCtxRef);
-                    io_uring_prep_splice(sqePipeRead, wrtCtxRef->openFd, -1, pipeFds[1], -1, length, SPLICE_F_MORE);
+                    io_uring_prep_splice(sqePipeRead, wrtCtxRef->openFd, -1, pipeFds[1], -1, SIXTYFOUR_KB, SPLICE_F_MORE);
                 
                     io_uring_sqe* sqePipeWrite = io_uring_get_sqe(&ring);
                     sqePipeWrite->flags = IOSQE_IO_LINK;
-                    io_uring_prep_splice(sqePipeWrite, pipeFds[0], 0, ctx->outgoingFd, -1, length, SPLICE_F_MORE);
+                    io_uring_prep_splice(sqePipeWrite, pipeFds[0], 0, ctx->outgoingFd, -1, SIXTYFOUR_KB, SPLICE_F_MORE);
 
                     wrtCtxRef->pipes.push_back({pipeFds[0], pipeFds[1]});
                 
@@ -332,14 +338,16 @@ int http2PushService::dataWrite(nghttp2_session *session, nghttp2_frame *frame, 
                         //absolute neanderthals
                         int* pipeFds = pipeMgr.getPipe();
                         io_uring_sqe* sqePipeRead = io_uring_get_sqe(&ring);
-                        io_uring_prep_splice(sqePipeRead, wrtCtxRef->openFd, -1, pipeFds[1], -1, length, 0);
+                        io_uring_prep_splice(sqePipeRead, wrtCtxRef->openFd, -1, pipeFds[1], -1, x, 0);
                 
                         io_uring_sqe* sqePipeWrite = io_uring_get_sqe(&ring);
-                        io_uring_prep_splice(sqePipeWrite, pipeFds[0], 0, ctx->outgoingFd, -1, length, 0);
+                        io_uring_prep_splice(sqePipeWrite, pipeFds[0], 0, ctx->outgoingFd, -1, x, 0);
                     }
 
                 }
             }
+        }else{
+
         }
     }    
 
